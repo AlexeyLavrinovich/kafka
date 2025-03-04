0bf20c54d943927a99cccbd96eb7b1ba95dca864
AlexeyLavrinovich <lavrinleha95@gmail.com>
2025-03-04 17:29:42 +0300

    Add producer and consumer

--- consumer/src/main/java/com/kafka/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/kafka/consumer/ConsumerApplication.java
@@ -2,12 +2,21 @@ package com.kafka.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class ConsumerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+    @KafkaListener(
+            topics = "experiment",
+            groupId = "group-1"
+    )
+    public void listener(String data) {
+        System.out.println(data);
+    }
 
 }
---
+++ producer/src/main/java/com/kafka/producer/controller/MessageController.java
@@ -0,0 +1,25 @@
+package com.kafka.producer.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/send")
+public class MessageController {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public MessageController(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping
+    public ResponseEntity sendMessage(@RequestBody String message) {
+        kafkaTemplate.send("experiment", message);
+        return ResponseEntity.ok("Message successfully sent!");
+    }
+}
